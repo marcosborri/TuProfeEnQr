f70424af589d940fe6f248f434b40a5a89166c7c
marcosborri <marcosborri06@gmail.com>
2026-04-12 18:53:17 -0300

    first commit

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,10 +1,15 @@
 <h1>Vista de Home</h1>
 
-<?php foreach($exercise as $exercise): ?>
+
+<?php foreach ($exercise as $exercise): ?>
     <h1><?= $exercise->title ?></h1>
 
     <div class="text"><?= $exercise->content ?></div>
 
     <img src="<?= ROOT ?><?= $exercise->image ?>">
+
+    <?php if ($_SESSION['USER']): ?>
+        <button type="button">Eliminar</button>
+    <?php endif?>
 
 <?php endforeach ?>
