32b85f6d16b38926405c7ccfc1e33b283928f32d
Chulo096 <1lucho2cavero3@gmail.com>
2026-04-14 20:31:39 -0300

    exercise cards css

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,25 +1,26 @@
 <?php require 'partials/header.php'?>
+<main class = "main">
+    <section class = "exercises-container">
+        <?php foreach ($exercise as $item): ?>
+            <div class="exercise-card">
+                <h1><?= $item->title ?></h1>
 
-<h1>Vista de Home</h1>
+                <div class="text"><?= $item->content ?></div>
 
+                <img src="<?= ROOT ?><?= $item->image ?>">
 
-<?php foreach ($exercise as $item): ?>
-    <h1><?= $exercise->title ?></h1>
+                <?php if ($_SESSION['USER']): ?>
+                    <form method="POST" action="<?= ROOT ?>/home/delete">
+                        <input type="hidden" name="id" value="<?= $item->id ?>">
 
-    <div class="text"><?= $item->content ?></div>
+                        <button type="submit" onclick="return confirm('¿Seguro que querés eliminar este ejercicio?')">
+                            Eliminar
+                        </button>
+                    </form>
+                <?php endif?>
+            </div>
 
-    <img src="<?= ROOT ?><?= $item->image ?>">
-
-    <?php if ($_SESSION['USER']): ?>
-        <form method="POST" action="<?= ROOT ?>/home/delete">
-            <input type="hidden" name="id" value="<?= $item->id ?>">
-
-            <button type="submit" onclick="return confirm('¿Seguro que querés eliminar este ejercicio?')">
-                Eliminar
-            </button>
-        </form>
-    <?php endif?>
-
-<?php endforeach ?>
-
+        <?php endforeach ?>
+    </section>
+</main>
 <?php require 'partials/footer.php'?>
