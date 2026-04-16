3c96ef96fe8948c54669c8a702fc387544880080
Chulo096 <1lucho2cavero3@gmail.com>
2026-04-16 18:41:20 -0300

    unfinished-css

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -1,14 +1,14 @@
+<link rel="stylesheet" href="<?= ROOT ?>/assets/css/login.css">
 <?php require 'partials/header.php'?>
 
-<form method="POST">
+<form class="login-form" method="POST">
 
-    <label for="email">Email:</label>
-    <input type="email" name="email" placeholder="ingrese su email">
+    <label class="form-email-label" for="email">Email:</label>
+    <input class="form-email-input" type="email" name="email" placeholder="ingrese su email">
 
-    <label for="password">Contraseña:</label>
-    <input type="password" name="password" placeholder="ingrese su contraseña">
-
-    <button type="submit">Ingresar</button>
+    <label class="form-password-label" for="password">Contraseña:</label>
+    <input class="form-password-input" type="password" name="password" placeholder="ingrese su contraseña">
+    <button class="form-submit-button" type="submit">Ingresar</button>
 
 </form>
 
