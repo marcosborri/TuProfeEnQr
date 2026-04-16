a125538b2d9e60c66c16e465fb4fb8aab4af526d
Chulo096 <1lucho2cavero3@gmail.com>
2026-04-16 19:36:24 -0300

    contact-me-button

--- app/views/partials/footer.php
+++ app/views/partials/footer.php
@@ -1,9 +1,27 @@
+<?php
+// Separamos el email en partes para protegerlo de los bots
+// 'nombre de email' y 'gmail.com'
+$parts = explode('@', CONTACT_EMAIL);
+?>
 
-  <footer>
-    <p>
-      contacto: tuprofeenqr@gmail.com
+<footer>
+  <p id="email-container">
     </p>
-  </footer>
-  
-</body>
-</html>
+</footer>
+
+<script>
+  (function() {
+    const user = "<?= $parts[0] ?>";
+    const domain = "<?= $parts[1] ?>";
+    
+
+    const container = document.getElementById('email-container');
+    const link = document.createElement('a');
+    
+    link.href = `mailto:${user}@${domain}`;
+    link.className = 'contact-link';
+    link.innerText = 'Contactame';
+    
+    container.appendChild(link);
+  })();
+</script>
